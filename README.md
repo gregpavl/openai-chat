a1401c9c7e8d7ab58d38b073c41a3f1399f630de
Grigoris Pavlidis <greg@ginbits.com>
2023-03-22 18:32:33 +0100

    init commit
    Signed-off-by: Grigoris Pavlidis <greg@ginbits.com>

---
+++ README.md
@@ -0,0 +1,47 @@
+# OpenAI API Chatbot
+
+This is a Go program that uses the OpenAI API to generate a response to a given text prompt.
+
+## Installation
+
+Clone the repository:
+
+```bash
+git clone <https://github.com/gregpavl/openai-chatbot.git>
+cd openai-chatbot
+```
+
+## Install the dependencies
+
+```bash
+go mod download
+```
+
+## Usage
+
+You need to obtain an API key from OpenAI. You can find the instructions on how to get the API key here.
+
+Once you have the API key, you can run the program with the following command:
+
+```bash
+go run main.go -apiKey=<your_api_key>
+```
+
+You can also customize the behavior of the chatbot by passing additional options:
+
+- `-model` : the OpenAI model ID to use (default: "gpt-3.5-turbo")
+- `-prompt` : the text prompt to generate a response to (default: "Write me a 100 word paragraph, use h1 and h2 and bold. reply in markdown format")
+- `-maxTokens` : the maximum number of tokens to generate in the response (default: 50)
+- `-temperature` : the sampling temperature for the model (default: 0.5)
+- `-frequencyPenalty` : the frequency penalty for the model (default: 0.5)
+- `-presencePenalty` : the presence penalty for the model (default: 0.5)
+
+For example:
+
+```bash
+go run main.go -apiKey=<your_api_key> -prompt="Write me a haiku" -maxTokens=20
+```
+
+## License
+
+This code is released under the MIT License.
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Request struct {
+	Model            string              `json:"model"`
+	Messages         []map[string]string `json:"messages"`
+	MaxTokens        int                 `json:"max_tokens"`
+	Temperature      float32             `json:"temperature"`
+	FrequencyPenalty float32             `json:"frequency_penalty"`
+	PresencePenalty  float32             `json:"presence_penalty"`
+}
+
+type Response struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Usage   struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+	Choices []struct {
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		FinishReason string `json:"finish_reason"`
+		Index        int    `json:"index"`
+	} `json:"choices"`
+	Error struct {
+		Message string `json:"message"`
+		Code    int    `json:"code"`
+		Type    string `json:"type"`
+	} `json:"error"`
+}
+
+type options struct {
+	apiKey           string
+	model            string
+	prompt           string
+	maxTokens        int
+	temperature      float64
+	frequencyPenalty float64
+	presencePenalty  float64
+}
+
+func gatherOptions() options {
+	o := options{}
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	fs.StringVar(&o.apiKey, "apiKey", "", "OpenAI API key")
+	fs.StringVar(&o.model, "model", "gpt-3.5-turbo", "OpenAI model ID")
+	fs.StringVar(&o.prompt, "prompt", "Write me a 100 word paragraph, use h1 and h2 and bold. reply in markdown format", "Text prompt to generate a response to")
+	fs.IntVar(&o.maxTokens, "maxTokens", 50, "Maximum number of tokens to generate in the response")
+	fs.Float64Var(&o.temperature, "temperature", 0.5, "Sampling temperature for the model")
+	fs.Float64Var(&o.frequencyPenalty, "frequencyPenalty", 0.5, "Frequency penalty for the model")
+	fs.Float64Var(&o.presencePenalty, "presencePenalty", 0.5, "Presence penalty for the model")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		logrus.WithError(err).Fatal("couldn't parse arguments.")
+	}
+	return o
+}
+
+func main() {
+	o := gatherOptions()
+
+	reqData := Request{
+		Model:            o.model,
+		Messages:         []map[string]string{{"role": "user", "content": o.prompt}},
+		MaxTokens:        o.maxTokens,
+		Temperature:      float32(o.temperature),
+		FrequencyPenalty: float32(o.frequencyPenalty),
+		PresencePenalty:  float32(o.frequencyPenalty),
+	}
+
+	reqBody, err := json.Marshal(reqData)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(reqBody))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", o.apiKey))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var respData Response
+	err = json.Unmarshal(respBody, &respData)
+	if err != nil {
+		panic(err)
+	}
+
+	if respData.Error.Message != "" {
+		panic(fmt.Sprintf("OpenAI API returned an error: %s (code %d, type %s)", respData.Error.Message, respData.Error.Code, respData.Error.Type))
+	}
+
+	if len(respData.Choices) == 0 {
+		panic("OpenAI API did not return any text")
+	}
+
+	fmt.Printf("Generated text: %s\n", respData.Choices[0].Message.Content)
+}
